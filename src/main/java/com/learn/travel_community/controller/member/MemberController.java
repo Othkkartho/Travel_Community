4e5c8f8cf572d3e3dbed2670299772571a68cc6b
qhtkdrma <kimwonkyu15@gmail.com>
2023-10-09 17:54:30 +0900

    프로필 보기 회원 정보 나오는 것 구현 완료

--- src/main/java/com/learn/travel_community/controller/member/MemberController.java
+++ src/main/java/com/learn/travel_community/controller/member/MemberController.java
@@ -6,6 +6,7 @@ import com.learn.travel_community.domain.member.MemberRepository;
 import com.learn.travel_community.dto.member.MemberDto;
 import com.learn.travel_community.service.member.MemberService;
 import jakarta.servlet.http.HttpSession;
+import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -21,6 +22,22 @@ public class MemberController {
     private final MemberRepository memberRepository;
     private final MemberService memberService;
     private final HttpSession httpSession;
+
+    @GetMapping(value="/profile")
+    public String getMyProfile(Model model) {
+        Member member = memberRepository.findByEmail(((SessionMember) httpSession.getAttribute("user")).getEmail()).orElse(null);
+        model.addAttribute("member", member);
+
+        return "/member/profile";
+    }
+
+    @GetMapping(value="/profile/{uid}")
+    public String getOthersProfile(@PathVariable String uid, Model model) {
+        Member member = memberRepository.findById(Long.parseLong(uid)).orElse(null);
+        model.addAttribute("member", member);
+
+        return "/member/profile";
+    }
 
     @GetMapping(value="/edit")
     public String getUsers(Model model) {
