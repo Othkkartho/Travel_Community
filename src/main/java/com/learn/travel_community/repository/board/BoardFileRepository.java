f5fe7768fafcb54552838ad637488500b47187ae
qhtkdrma <kimwonkyu15@gmail.com>
2023-10-19 21:51:24 +0900

    Boarder 파일들 제거

--- src/main/java/com/learn/travel_community/repository/board/BoardFileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.learn.travel_community.repository.board;
-
-import com.learn.travel_community.domain.board.BoardFileEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardFileRepository extends JpaRepository<BoardFileEntity, Long> {
-}
