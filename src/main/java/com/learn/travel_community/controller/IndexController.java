52ba688f5ab73df3fe1184b6bf8089715c878e48
minseoBae <minseo2399@naver.com>
2023-11-20 03:50:04 +0900

    추천 게시글 메인에 나이대, 연령 별로 띄우기 구현 완료, 클릭시 해당 추천 게시글로 이등 구현 중

--- src/main/java/com/learn/travel_community/controller/IndexController.java
+++ src/main/java/com/learn/travel_community/controller/IndexController.java
@@ -1,25 +1,42 @@
 package com.learn.travel_community.controller;
 
 import com.learn.travel_community.config.member.oauth.dto.SessionMember;
-import com.learn.travel_community.domain.member.Member;
+import com.learn.travel_community.domain.member.MemberRepository;
+import com.learn.travel_community.dto.board.BoardRecommendDto;
+import com.learn.travel_community.service.board.boardRecommendService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final HttpSession httpSession;
+    private final boardRecommendService boardRecommendService;
+    private final MemberRepository memberRepository;
 
     @GetMapping("/")
     public String index(Model model) {
         SessionMember member = (SessionMember) httpSession.getAttribute("user");
+        int age = 20;
+        int gender = 1;
+
         if (member != null) {
+            age = memberRepository.findByEmail(member.getEmail()).get().getAge();
+            gender = memberRepository.findByEmail(member.getEmail()).get().getGender();
+            if(gender == 0) { gender = 1; }
             model.addAttribute("userName", member.getNickname());
             model.addAttribute("profileImg", member.getPicture());
         }
+
+        List<BoardRecommendDto> boardRecommendDtos = boardRecommendService.findAllByAgeAndGender(age, gender);
+        model.addAttribute("boardRecommendDTOs", boardRecommendDtos);
 
         return "index";
     }
