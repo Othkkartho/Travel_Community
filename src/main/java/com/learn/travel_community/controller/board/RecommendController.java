0858ab3096772c6fa6a438ba81e8c869e90cdeef
minseoBae <minseo2399@naver.com>
2023-11-20 14:33:44 +0900

    댓글 없는 게시글 디테일 오류 수정, 게시글 디테일 페이지 사진 나올 수 있게 변경

--- src/main/java/com/learn/travel_community/controller/board/RecommendController.java
+++ src/main/java/com/learn/travel_community/controller/board/RecommendController.java
@@ -1,27 +1,31 @@
 package com.learn.travel_community.controller.board;
 
-import com.learn.travel_community.domain.member.MemberRepository;
+import com.learn.travel_community.config.member.oauth.dto.SessionMember;
 import com.learn.travel_community.dto.board.BoardRecommendDto;
 import com.learn.travel_community.service.board.boardRecommendService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping(name = "/recommend")
 public class RecommendController {
     private final boardRecommendService boardRecommendService;
+    private final HttpSession httpSession;
 
     @Transactional
-    @GetMapping("/community/Community_recommend/{id}")
-    public ModelAndView detail(@PathVariable Long id) {
+    @GetMapping("/{id}")
+    public String detail(@PathVariable Long id, Model model) {
+        SessionMember member = (SessionMember) httpSession.getAttribute("user");
+
         BoardRecommendDto boardRecommendDTO = boardRecommendService.findOne(id);
-        ModelAndView modelAndView = new ModelAndView("Community_recommend");
-        modelAndView.addObject("boardRecommendDTO", boardRecommendDTO);
-        return modelAndView;
+        model.addAttribute("boardRecommendDTO", boardRecommendDTO);
+        return "/community/Community_recommend";
     }
 }
